d62276515f890521833c09abf8aac7e436991d46
SantosMaycon <mayconalves.santos14@gmail.com>
2023-01-13 00:30:52 -0200

    recycling the platforms

--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class SpawnPlatform : MonoBehaviour {
   [SerializeField] private List<GameObject> platforms = new List<GameObject>();
+  private List<Transform> currentPlatforms = new List<Transform>();
   private float distance = 0f;
+  private GameObject player;
+  private Transform currentPlatformPoint;
+  private int platformIndex;
   // Start is called before the first frame update
   void Start() {
+    player = GameObject.FindGameObjectWithTag("Player");
+
     foreach(GameObject platform in platforms) {
-      Instantiate(platform, new Vector2(distance, 0f), transform.rotation);
+      GameObject obj = Instantiate(platform, new Vector2(distance, 0f), transform.rotation);
+      currentPlatforms.Add(obj.transform);
       distance += 30;
-    } 
+    }
+
+    currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
   }
 
   // Update is called once per frame
-  void Update() {}
+  void Update() {
+    move();
+  }
+
+  void move() {
+    float distanceBetweenPlayerAndFinalPoint = player.transform.position.x - currentPlatformPoint.position.x;
+
+    if(distanceBetweenPlayerAndFinalPoint >= 1) {
+      recycle(currentPlatforms[platformIndex].gameObject);
+      platformIndex = platformIndex < currentPlatforms.Count - 1 ? ++platformIndex : 0;
+      currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
+    }
+  }
 
   public void recycle(GameObject platform) {
     platform.transform.position = new Vector2(distance, 0f);
