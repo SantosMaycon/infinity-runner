017d767390ea3567384e325ac2209a7e5f2a8747
SantosMaycon <mayconalves.santos14@gmail.com>
2023-01-14 17:37:31 -0200

    add shoot by player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour {
   [SerializeField] private float speed;
   [SerializeField] private float jumpFoarce;
+  [SerializeField] private GameObject bulletPrefab;
+  [SerializeField] private Transform pointOfShoot;
   [SerializeField] private Animator animator;
   private Rigidbody2D rigidbody2d;
   private bool isJumping;
@@ -15,10 +17,15 @@ public class Player : MonoBehaviour {
 
   // Update is called once per frame
   void Update() {
-    if (Input.GetKeyDown(KeyCode.Space) && !isJumping) {
+    if (Input.GetKeyDown(KeyCode.UpArrow) && !isJumping) {
       rigidbody2d.AddForce(Vector2.up * jumpFoarce, ForceMode2D.Impulse);
       animator.SetBool("isJump", true);
       isJumping = true;
+    }
+
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      var bullet = Instantiate(bulletPrefab, pointOfShoot.position, Quaternion.identity);
+      Destroy(bullet, 5f);
     }
   }
 
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+  [SerializeField] private float speed;
+  private Rigidbody2D rigidbody2d;
+  // Start is called before the first frame update
+  void Start() {
+    rigidbody2d = GetComponent<Rigidbody2D>();
+    rigidbody2d.velocity = Vector2.right * speed;
+  }
+
+  // Update is called once per frame
+  void Update() {}
+}
